c5037a996da4826e0df5e8ebe09d32aa1365cad6
kitamas <kitamas25@gmail.com>
2022-10-13 12:23:11 +0200

    commit

--- get_events.py
+++ get_events.py
@@ -80,8 +80,8 @@ def main():
     response = service.calendarList().list().execute()
     print("RESPONSE", response)
     print(response.keys())
-    print(response.get('items'))
-    print(response.get('items')[0])
+    print("RESPONSE ITEMS",response.get('items'))
+
     # try:
     page_token = None
     #calendar_ids = ['61u5i3fkss34a4t50vr1j5l7e4@group.calendar.google.com','r0evkror5p88vkhf3q842jk8fg@group.calendar.google.com']
