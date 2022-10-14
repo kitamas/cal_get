5b12121717b33fb0082b99aa8f7da71f89f6028b
kitamas <kitamas25@gmail.com>
2022-10-14 07:37:12 +0200

    commit

--- get_events.py
+++ get_events.py
@@ -87,6 +87,7 @@ def main():
             for calendar_list_entry in calendar_list['items']:
                 if '@group.calendar.google.com' in calendar_list_entry['id']:
                     calendar_ids.append(calendar_list_entry['id'])
+                    print("FOR FOR",calendar_list_entry['id'])
             page_token = calendar_list.get('nextPageToken')
             if not page_token:
                 break
@@ -98,6 +99,7 @@ def main():
         for calendar_id in calendar_ids:
             count = 0
             print('\n----%s:\n' % calendar_id)
+            print("FOR 2 FOR 2",calendar_id)
             events_result = service.events().list(
                 calendarId=calendar_id,
                 timeMin=start_date,
