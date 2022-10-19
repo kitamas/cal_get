14288df72a1a6a88cc29d30f345341c00d4a5dbb
kitamas <kitamas25@gmail.com>
2022-10-19 17:09:33 +0200

    commit

--- get_events.py
+++ get_events.py
@@ -128,12 +128,12 @@ def main():
 
             events_cal2 = "CAL2: " 
             for event in events:
-"""
+                """
                 time_cal2_Z = event['start'].get('dateTime', event['start'].get('date'))
                 # 2022-10-15T10:00:00Z
                 # time_cal2 = datetime.datetime.strptime(time_cal2_Z,'%Y-%m-%dT%H:%M:%S%z')
                 events_cal2 +=  event['summary'] + " | " + time_cal2_Z + " | "
-"""
+                """
                 time_cal2_ISO = event['start'].get('dateTime', event['start'].get('date'))
 
                 time_cal2_obj = datetime.datetime.fromisoformat(time_cal2_ISO)
