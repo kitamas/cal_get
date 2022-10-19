8743e43372c6e688fdb53cee788517a9aa5e73ff
kitamas <kitamas25@gmail.com>
2022-10-19 16:47:44 +0200

    commit

--- get_events.py
+++ get_events.py
@@ -198,8 +198,8 @@ def findFirstOpenSlot(events,startTime,endTime,duration):
         return startTime
 
     for i, gap in enumerate(gaps):
-        if gap >= duration:
-        #if gap > duration:
+        #if gap >= duration:
+        if gap > duration:
             #This means that a gap is bigger than the desired slot duration, and we can "squeeze" a meeting.
             #Just after that meeting ends.
             return eventEnds[i]
