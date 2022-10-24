f63eb77cf7183603a99781b1ab633e76e00ea765
kitamas <kitamas25@gmail.com>
2022-10-24 16:29:24 +0200

    commit

--- get_events.py
+++ get_events.py
@@ -184,15 +184,16 @@ def findFirstOpenSlot(events,startTime,endTime,duration):
     eventStarts = [parseDate(e['start'].get('dateTime', e['start'].get('date'))) for e in events]
 
     eventEnds = [parseDate(e['end'].get('dateTime', e['end'].get('date'))) for e in events]
-    print("EVENTENDS =", eventEnds)
-    print("EVENTENDS[0] =", eventEnds[0])
+    # eventEnds = [datetime.datetime(2022, 10, 24, 18, 0), datetime.datetime(2022, 10, 24, 20, 0), datetime.datetime(2022, 10, 24, 22, 0)]
+    # eventEnds[0] = 2022-10-24 18:00:00
     gaps = [start-end for (start,end) in zip(eventStarts[1:], eventEnds[:-1])]
 
     print("start = eventStarts = ",eventStarts,"end = eventEnds = ", eventEnds,"gaps = ",gaps)
 
     print("FIRST OPEN START = ",eventStarts[0],"FIRST OPEN END =",eventEnds[0])
+    # FIRST OPEN START =  2022-10-24 17:00:00 FIRST OPEN END = 2022-10-24 18:00:00
 
-    #  start = eventStarts =  [datetime.datetime(2022, 10, 19, 15, 0), datetime.datetime(2022, 10, 19, 17, 0), datetime.datetime(2022, 10, 19, 19, 0)] end = eventEnds =  [datetime.datetime(2022, 10, 19, 16, 0), datetime.datetime(2022, 10, 19, 18, 0), datetime.datetime(2022, 10, 19, 20, 0)] gaps =  [datetime.timedelta(seconds=3600), datetime.timedelta(seconds=3600)]
+    #  start = eventStarts, end = eventEnds, gaps =  [datetime.timedelta(seconds=3600), datetime.timedelta(seconds=3600)]
 
     # if ROUNDED startTime + duration < eventStarts[0]:
     if startTime + duration < eventStarts[0]:
